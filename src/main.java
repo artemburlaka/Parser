9d3adf36b143df47ef5f3e0ac8e5b10cc97568cc
Artem Burlaka <guestartem@gmail.com>
2016-08-25 22:26:15 +0300

    all GK search

--- src/main.java
+++ src/main.java
@@ -1,5 +1,57 @@
 /**
  * Created by admin on 25.08.2016.
  */
-public class main {
+import java.io.File;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+/**
+ * Java Program to parse/read HTML documents from File using Jsoup library.
+ * Jsoup is an open source library which allows Java developer to parse HTML
+ * files and extract elements, manipulate data, change style using DOM, CSS and
+ * JQuery like method.
+ *
+ * @author Javin Paul
+ */
+public class main{
+
+    public static void main(String args[]) {
+
+        // Parse HTML String using JSoup library
+        String HTMLSTring = "<!DOCTYPE html>"
+                + "<html>"
+                + "<head>"
+                + "<title>JSoup Example</title>"
+                + "</head>"
+                + "<body>"
+                + "<table><tr><td><h1>HelloWorld</h1></tr>"
+                + "</table>"
+                + "</body>"
+                + "</html>";
+
+        Document html = Jsoup.parse(HTMLSTring);
+        String title = html.title();
+        String gk = html.body().getElementsByTag("h1").text();
+
+        System.out.println("Input HTML String to JSoup :" + HTMLSTring);
+        System.out.println("After parsing, Title : " + title);
+        System.out.println("Afte parsing, Heading : " + gk);
+
+        // JSoup - Reading HTML page from URL
+        Document doc;
+        try {
+            doc = Jsoup.connect("http://ofmanager.net/spiel/14171.html").get();
+            title = doc.title();
+            gk = doc.body().getElementsContainingOwnText("GK").text();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Jsoup Can read HTML page from URL, gk : " + gk);
+
+
+    }
 }
